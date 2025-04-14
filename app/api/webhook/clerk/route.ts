feeb9369e677377241e4ad436b536e5b5ab94bf5
harish-pasupuleti <harishpasupuleti18@gmail.com>
2025-04-14 20:03:20 +0530

    deleted unwanted

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,9 +1,12 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import { getDbConnection } from "@/lib/db";
+import { createOrUpdateUser } from "@/lib/user";
 
 export async function POST(req: Request) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -12,23 +15,28 @@ export async function POST(req: Request) {
     );
   }
 
-  const headerPayload = headers();
+  // Get the headers
   const svix_id = req.headers.get("svix-id") as string | null;
   const svix_timestamp = req.headers.get("svix-timestamp") as string | null;
   const svix_signature = req.headers.get("svix-signature") as string | null;
   
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occurred -- no svix headers", {
+    return new Response("Error occured -- no svix headers", {
       status: 400,
     });
   }
 
+  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
+  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
+
   let evt: WebhookEvent;
 
+  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -37,48 +45,41 @@ export async function POST(req: Request) {
     }) as WebhookEvent;
   } catch (err) {
     console.error("Error verifying webhook:", err);
-    return new Response("Error occurred", {
+    return new Response("Error occured", {
       status: 400,
     });
   }
 
+  // Do something with the payload
+  // For this guide, you simply log the payload to the console
   const { id } = evt.data;
   const eventType = evt.type;
 
-  console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", body);
-
-  // Handling 'user.created' event
   if (eventType === "user.created") {
-    try {
-      const {  email_addresses,primary_email_address_id ,  first_name, last_name } =
-      evt.data;      console.log(evt.data);
-      // Safely find the primary email address
-      const primaryEmail = email_addresses.find(
-        (email) => email.id === primary_email_address_id
-      );
-      console.log("Primary email:", primaryEmail);
-      console.log("Email addresses:", primaryEmail?.email_address);
+    const { id, email_addresses,  first_name, last_name, username } =
+      evt.data;
 
-      if (!primaryEmail) {
-        console.error("No primary email found");
-        return new Response("No primary email found", { status: 400 });
-      }
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      name: username!,
+      firstName: first_name,
+      lastName: last_name,
+    };
 
-      // Create the user in the database
-      const newUser = await prisma.user.create({
-        data: {
-          id: evt.data.id!,
-          email: primaryEmail.email_address,
-         full_name:`${first_name} ${last_name}`
-        },
-      });
-      console.log("New user created:", newUser);
-    } catch (error) {
-      console.error("Error creating user in database:", error);
-      return new Response("Error creating user", { status: 500 });
-    }
+    const sql = await getDbConnection();
+
+    await createOrUpdateUser({
+      sql,
+      id: user.clerkId,
+      email: user.email,
+      full_name: `${user.firstName} ${user.firstName}`,
+    });
+    return NextResponse.json({ message: "New user created", user });
   }
 
-  return new Response("Webhook received successfully", { status: 200 });
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log("Webhook body:", body);
+
+  return new Response("", { status: 200 });
 }
